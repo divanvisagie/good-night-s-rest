b9070cf19cd87aa97643a2448a9872dbc6f242e0
Divan visagie <me@dvisagie.com>
2023-10-13 20:54:55 +0200

    Fix breaking change

--- src/components/text.rs
+++ src/components/text.rs
@@ -1,36 +1,22 @@
+use eframe::egui::{TextEdit, Ui};
 
-use eframe::egui::{TextEdit, Ui, TextStyle};
-use std::option::Option;
-
-pub struct TextInput<'a> {
+pub struct TextInput <'a> {
     value: &'a mut String,
     title: &'a str,
-    text_style: Option<TextStyle>,
 }
 
-impl<'a> TextInput<'a> {
-    pub fn new(title: &'a str, value: &'a mut String, text_style: Option<TextStyle>) -> TextInput<'a> {
+impl <'a> TextInput <'a> {
+    pub fn new(title: &'a str, value: &'a mut String) -> TextInput <'a> {
         TextInput {
             title,
-            value,
-            text_style,
+            value
         }
     }
 
     pub fn show(&mut self, ui: &mut Ui) {
         ui.vertical(|ui| {
             ui.label(self.title);
-
-            if let Some(style) = self.text_style {
-                ui.style_mut().body_text_style = style;
-            }
-
             ui.add(TextEdit::singleline(self.value));
-
-            // Reset to default text style if changed
-            if self.text_style.is_some() {
-                ui.style_mut().body_text_style = TextStyle::Body;
-            }
         });
     }
 }
